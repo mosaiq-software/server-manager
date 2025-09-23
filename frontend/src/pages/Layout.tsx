38c0c5c945456ff58ea78404ba17bc72adc87d61
asantagata <asantagata225@gmail.com>
2025-09-22 22:19:51 -0400

    fix to sidebar

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -164,7 +164,9 @@ const Layout = (props: { children: React.ReactNode }) => {
                     </Menu>
                 </AppShell.Header>
 
-                <AppShell.Navbar p="md">
+                <AppShell.Navbar p="md" style={{
+                    height: `calc(100dvh - var(--app-shell-header-height) - 1px)`
+                }}>
                     <RouterLink
                         to="/"
                         label="Dashboard"
@@ -217,6 +219,7 @@ const Layout = (props: { children: React.ReactNode }) => {
                     <Button
                         onClick={() => setModal('create')}
                         variant="outline"
+                        style={{flexShrink: '0'}}
                     >
                         Create Project
                     </Button>
